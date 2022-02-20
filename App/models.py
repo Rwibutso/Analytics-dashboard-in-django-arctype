6ef0472ec221e031e83f6b400193737cdaaa349c
Serge <rwibuserge@gmail.com>
2022-02-20 16:53:59 +0200

    updated

--- App/models.py
+++ App/models.py
@@ -1,36 +1,38 @@
 from django.db import models
+from django.utils import timezone
+
 import datetime
 
 # Create your models here.
 class Analytics(models.Model):
     name=name = models.TextField()
     stats= models.IntegerField(default=0)
-    date= models.DateTimeField(default=datetime.datetime.now())
+    date= models.DateTimeField(default=timezone.now)
 
 class Event(models.Model):
     name = models.TextField()
     username = models.TextField()
     ip = models.CharField(max_length=40)
     session = models.CharField(max_length=40)
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField(default=timezone.now)
 
 class RegisterEvent(models.Model):
     username = models.TextField()
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField(default=timezone.now)
 
 class LoginEvent(models.Model):
     username = models.TextField()
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField(default=timezone.now)
 
 class ViewPageEvent(models.Model):
     ip = models.CharField(max_length=40)
     session = models.CharField(max_length=40)
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField(default=timezone.now)
 
 class EditProfileEvent(models.Model):
     username = models.TextField()
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField(default=timezone.now)
 
 class LogoutEvent(models.Model):
     username = models.TextField()
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField(default=timezone.now)
--- Arctype_Dashboard/settings.py
+++ Arctype_Dashboard/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
